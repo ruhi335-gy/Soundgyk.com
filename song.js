e8028beb575a4635c4d698f6ef0b105da2f08c31
ruhi335-gy <gyangupta16368@gmail.com>
2025-12-05 15:00:11 +0530

    Add files via upload

---
+++ song.js
@@ -0,0 +1,23 @@
+const songs = [
+  {
+    id: 1,
+    title: "Brack my rull",
+    artist: "SOUNDGYK",
+    img: "3.png",
+    src: "3.mp3"
+  },
+  {
+    id: 2,
+    title: "You're My only one",
+    artist: "SOUNDGYK",
+    img: "2.png",
+    src: "2.mp3"
+  },
+  {
+    id: 3,
+    title: "Lost in the night",
+    artist: "SOUNDGYK",
+    img: "3.png",
+    src: "3.mp3"
+  }
+];
